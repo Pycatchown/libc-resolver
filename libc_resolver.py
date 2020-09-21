0ec2ef9bc1391f65a3d89ff64513a841d1d6b5f9
Pycatchown <b.mayaud-shaiya@live.fr>
2020-09-22 00:13:12 +0200

    Better logs

--- libc_resolver.py
+++ libc_resolver.py
@@ -8,6 +8,7 @@ def libc_resolve(dict_sym_addr, choice=0):
     if len(dict_sym_addr) <= 1:
         log.warning_once("[libc-resolver]: No reliable result is guaranteed without at least two symbols")
     result = libcdb_wrapper.find(dict_sym_addr)
+    log.info("Found:\n%s" % result)
     regex_lib_names = r"\((.*)\)"
     libs = re.findall(regex_lib_names, result, re.MULTILINE)
     if len(libs) > 1:
